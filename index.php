7f997fed64b7c6d13b260246acd12c14dd5b6adc
insphere_mayank <insphere.mayank@gmail.com>
2018-03-23 11:25:42 +0530

    logo added

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Begin global header section -->
+<?php
+$pageTitle = 'Red Shield';
+include("includes/_global-Head.php");
+?>
+
+
+
+
+
+<?php
+    include("includes/_footer.php");
+?>
+<div class="clearfix"></div>
+<!-- Begin Script section -->
+<?php
+include("includes/_scripts.php");
+?>
+<!-- Begin Script section -->
+
+</body>
+</html>
