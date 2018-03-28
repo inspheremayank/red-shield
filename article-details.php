ebdaefbee90831a6260313ef459e437505e28a7d
insphere_mayank <insphere.mayank@gmail.com>
2018-03-28 15:35:39 +0530

    cards updates

---
+++ article-details.php
@@ -0,0 +1,260 @@
+<!-- Begin global header section -->
+<?php
+$pageTitle = 'Red Shield';
+include("includes/_global-Head.php");
+?>
+<?php
+    include("includes/_header.php");
+?>
+<div class="clearfix"></div>
+<!-- Begin about Banner section markup -->
+<div class="banner__section image-covered text-center" style="background-image: url('static/images/test-image.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="banner__section-content d-flex align-items-center flex-column justify-content-center m-auto">
+                    <div class="play-icon"></div>
+                    <h1>The Salvation Army are there for those who need it most.</h1>
+                    <p>This is summary text or an excerpt of the post. It can be slightly longer or shorter if it needst to be.</p>
+                    <a href="javascript:;" class="button button--red upper button--radius">Donate</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End about Banner section markup -->
+<div class="clearfix"></div>
+<!-- Begin About CTA section -->
+<div class="cta-wrap cta-wrap__about">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="cta-wrap__main text-center">
+                    <div class="cta-wrap__main--title">The Red Shield Appeal</div>
+                    <div class="cta-wrap__main--description">Explainer  and emotive text about the appeal - why it matters and how you can help. unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End About CTA section -->
+<div class="clearfix"></div>
+<!-- Begin learning CTA section -->
+<div class="cta-wrap cta-wrap__learning bg-image" style="background-image: url('static/images/test-image.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="cta-wrap__main text-center">
+                    <div class="cta-wrap__main--sr">
+                        <div class="cta-wrap__main--title">Jen’s Story</div>
+                        <div class="cta-wrap__main--description">This is an excerpt from the story - generally it is 80 characters, but it can be longer. Finish with something that makes them want to click.</div>
+                        <div class="cta-wrap__main--buttons">
+                            <a class="button button--radius button--red" href="javascript:;">LEARN MORE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End learning CTA section -->
+<div class="clearfix"></div>
+<!-- Begin cards design section -->
+<div class="card-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3 class="text-center fs-52 font-light red--color">Our Community</h3>
+                <div class="row">
+                    <div class="card__view col-12 col-md-4 card__view-third">
+                        <div class="">
+                            <a href="javascript:;" class="card__view--items">
+                                <div class="card__view--content">
+                                    <div class="mask"></div>
+                                    <figure class="image-covered" style="background-image: url('static/images/test-image.jpg')"></figure>
+                                    <div class="optionSet">
+                                        <ul>
+                                            <li>Hide <span class="fa fa-eye-slash"></span></li>
+                                            <li>Pin <span class="fa fa-map-marker"></span></li>
+                                            <li>Edit <span class="fa fa-cog"></span></li>
+                                        </ul>
+                                    </div>
+                                    <div class="social-icons"></div>
+                                    <div class="content__section-play"></div>
+                                    <div class="content__section">
+                                        <div class="content__section-userInfo"> 
+                                            <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
+                                            <div class="content__section-userInfo-content"> 
+                                                <span>Rebekah Brown</span> 
+                                                <span class="designation">Director of Monash Sustainable Development Institute</span> 
+                                            </div> 
+                                        </div>
+                                        <div class="clearfix"></div>
+                                        <h2 class="content__section-heading">With Image card</h2>
+                                        <div class="content__section-description">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card__view col-12 col-md-4 card__view-third">
+                        <div class="social twitter">
+                            <a href="javascript:;" class="card__view--items">
+                                <div class="card__view--content">
+                                    <div class="mask"></div>
+                                    <figure class="image-covered" style="background-image: url('static/images/test-image.jpg')"></figure>
+                                    <div class="optionSet">
+                                        <ul>
+                                            <li>Hide <span class="fa fa-eye-slash"></span></li>
+                                            <li>Pin <span class="fa fa-map-marker"></span></li>
+                                            <li>Edit <span class="fa fa-cog"></span></li>
+                                        </ul>
+                                    </div>
+                                    <div class="social-icons"></div>
+                                    <div class="content__section-play"></div>
+                                    <div class="content__section">
+                                        <div class="content__section-userInfo"> 
+                                            <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
+                                            <div class="content__section-userInfo-content"> 
+                                                <span>Rebekah Brown</span> 
+                                                <span class="designation">Director of Monash Sustainable Development Institute</span> 
+                                            </div> 
+                                        </div>
+                                        <div class="clearfix"></div>
+                                        <h2 class="content__section-heading">Social cards</h2>
+                                        <div class="content__section-description">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card__view col-12 col-md-4 card__view-third">
+                        <div class="without-image">
+                            <a href="javascript:;" class="card__view--items">
+                                <div class="card__view--content">
+                                    <div class="mask"></div>
+                                    <figure class="image-covered" style="background-image: url('static/images/test-image.jpg')"></figure>
+                                    <div class="optionSet">
+                                        <ul>
+                                            <li>Hide <span class="fa fa-eye-slash"></span></li>
+                                            <li>Pin <span class="fa fa-map-marker"></span></li>
+                                            <li>Edit <span class="fa fa-cog"></span></li>
+                                        </ul>
+                                    </div>
+                                    <div class="social-icons"></div>
+                                    <div class="video-play"></div>
+                                    <div class="content__section">
+                                        <div class="content__section-userInfo"> 
+                                            <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
+                                            <div class="content__section-userInfo-content"> 
+                                                <span>Rebekah Brown</span> 
+                                                <span class="designation">Director of Monash Sustainable Development Institute</span> 
+                                            </div> 
+                                        </div>
+                                        <div class="clearfix"></div>
+                                        <h2 class="content__section-heading">Without Image</h2>
+                                        <div class="content__section-description">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card__view col-12 col-md-4 card__view-third">
+                        <div class="social twitter video">
+                            <a href="javascript:;" class="card__view--items">
+                                <div class="card__view--content">
+                                    <div class="mask"></div>
+                                    <figure class="image-covered" style="background-image: url('static/images/test-image.jpg')"></figure>
+                                    <div class="optionSet">
+                                        <ul>
+                                            <li>Hide <span class="fa fa-eye-slash"></span></li>
+                                            <li>Pin <span class="fa fa-map-marker"></span></li>
+                                            <li>Edit <span class="fa fa-cog"></span></li>
+                                        </ul>
+                                    </div>
+                                    <div class="social-icons"></div>
+                                    <div class="video-play"></div>
+                                    <div class="content__section">
+                                        <div class="content__section-userInfo"> 
+                                            <div class="content__section-userInfo-image image-covered" style="background-image: url('static/images/test-image.jpg')"></div> 
+                                            <div class="content__section-userInfo-content"> 
+                                                <span>Rebekah Brown</span> 
+                                                <span class="designation">Director of Monash Sustainable Development Institute</span> 
+                                            </div> 
+                                        </div>
+                                        <div class="clearfix"></div>
+                                        <h2 class="content__section-heading">Social cards</h2>
+                                        <div class="content__section-description">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End cards design section -->
+<div class="clearfix"></div>
+<!-- Begin CTA section -->
+<div class="cta-wrap cta-wrap__action blue-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="cta-wrap__main">
+                    <div class="cta-wrap__main--logo">
+                        <img class="img-fluid" src="/static/images/logos/salvation-logo.svg" alt="CTA logo" />
+                    </div>
+                    <div class="cta-wrap__main--title"></div>
+                    <div class="cta-wrap__main--description">Contribute to the appeal and change lives now. Your donation goes straight to those who need it most.</div>
+                    <div class="cta-wrap__main--buttons">
+                        <a class="button button--radius button--black" href="javascript:;">FUNDRAISE</a>
+                        <a class="button button--radius button--red" href="javascript:;">DONATE</a>
+                        <a class="button button--radius button--lightBlue" href="javascript:;">VOLUNTEER</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End CTA section -->
+<div class="clearfix"></div>
+<!-- Begin Subscribe CTA section -->
+<div class="cta-wrap cta-wrap__subscribe blue-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="cta-wrap__main">
+                    <div class="cta-wrap__main--sl">
+                        <div class="cta-wrap__main--title">Subscribe</div>
+                        <div class="cta-wrap__main--description">Sign up to our mailing list, and we will keep you updated on our programs and how your donation is doing good in the community.</div>
+                    </div>
+                    <div class="cta-wrap__main--sr">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" placeholder="your email" />
+                                <button class="button button--red" type="submit">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Subscribe CTA section -->
+<div class="clearfix"></div>
+
+<?php
+    include("includes/_footer.php");
+?>
+<div class="clearfix"></div>
+<!-- Begin Script section -->
+<?php
+include("includes/_scripts.php");
+?>
+<!-- Begin Script section -->
+
+</body>
+</html>
