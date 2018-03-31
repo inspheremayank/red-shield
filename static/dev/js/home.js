5b2cec5f9536f4025ad4bdfb47935f431c6c2bca
insphere-nitish <insphere.nitish@gmail.com>
2018-03-31 10:24:58 +0530

    fa spinner class added.

--- static/dev/js/home.js
+++ static/dev/js/home.js
@@ -429,7 +429,7 @@ HomeController.Listing = (function ($) {
                     }
                 },
                 beforeSend: function (jqXHR, settings) {
-                    $(btnObj).html('<i class="fa fa-refresh fa-spin fa-fw" aria-hidden="true"></i> Please wait...');
+                    $(btnObj).html('<button class="button button__icon--left button--blue button--radius"><i class="fa fa-spinner fa-spin"></i>Load more</button>');
                 },
                 onComplete: function (jqXHR, textStatus) {
                     $(btnObj).html('<i class="fa fa-arrow-down" aria-hidden="true"></i> Load More');
